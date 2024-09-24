6f3c27631cd52ca10e15767306a6b7b2a16379e1
atiilla <9992685+atiilla@users.noreply.github.com>
2024-09-24 13:08:35 +0300

    inheritance

---
+++ csharp/Nullables/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullables
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        public string getAge() => (DateTime.Now - BirthDate).ToString();
+
+        public override string ToString() => Name;
+
+    }
+
+    internal class Student : Person
+    {
+        public long studentNumber;
+        public Student(string name):base(name) { }
+
+        
+
+        public override string ToString() => base.ToString() + " " + studentNumber;
+    }
+}
--- csharp/Nullables/Program.cs
+++ csharp/Nullables/Program.cs
@@ -1,21 +1,24 @@
 ﻿// Create a new employee
 using Nullables;
+using static Nullables.Person;
 
-Employee employee = new Employee();
+//Employee employee = new Employee();
 
-Console.Write("Enter employee name: ");
-employee.Name = Console.ReadLine();
+//Console.Write("Enter employee name: ");
+//employee.Name = Console.ReadLine();
 
-Console.Write("Enter days worked in the year in Sales (if none, press Enter): ");
-string salesInput = Console.ReadLine();
-employee.SalesDuration = string.IsNullOrWhiteSpace(salesInput) ? 0 : float.Parse(salesInput);
+//Console.Write("Enter days worked in the year in Sales (if none, press Enter): ");
+//string salesInput = Console.ReadLine();
+//employee.SalesDuration = string.IsNullOrWhiteSpace(salesInput) ? 0 : float.Parse(salesInput);
 
-Console.Write("Enter days worked in the year in Support (if none, press Enter): ");
-string supportInput = Console.ReadLine();
-employee.SupportDuration = string.IsNullOrWhiteSpace(supportInput) ? 0 : float.Parse(supportInput);
+//Console.Write("Enter days worked in the year in Support (if none, press Enter): ");
+//string supportInput = Console.ReadLine();
+//employee.SupportDuration = string.IsNullOrWhiteSpace(supportInput) ? 0 : float.Parse(supportInput);
 
-Console.Write("Enter days worked in the year in Administration (if none, press Enter): ");
-string adminInput = Console.ReadLine();
-employee.AdminDuration = string.IsNullOrWhiteSpace(adminInput) ? 0 : float.Parse(adminInput);
+//Console.Write("Enter days worked in the year in Administration (if none, press Enter): ");
+//string adminInput = Console.ReadLine();
+//employee.AdminDuration = string.IsNullOrWhiteSpace(adminInput) ? 0 : float.Parse(adminInput);
 
-employee.DisplayEmployeeDetails();
+//employee.DisplayEmployeeDetails();
+
+Student student1 = new Student();
