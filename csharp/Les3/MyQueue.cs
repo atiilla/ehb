ff5978e08026e0447be1394d81ab1ee134c212b0
atiilla <9992685+atiilla@users.noreply.github.com>
2024-09-24 11:54:29 +0300

    * Generic Class * List<T> * Queue

---
+++ csharp/Les3/MyQueue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les3
+{
+    internal class MyQueue<T>
+    {
+        List<T> list = new List<T>();
+        public MyQueue() {
+           
+        }
+
+        public void Add(T item) {
+            list.Add(item);
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+        public T NextOut() {
+            T first = list[0];
+            list.RemoveAt(0);
+            return first;
+
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+            for(int i=0; i<list.Count; i++)
+            {
+                output += "\n" + list[i].ToString();
+            }
+            return output;
+        }
+
+        
+    }
+}
--- csharp/Les3/Program.cs
+++ csharp/Les3/Program.cs
@@ -1,7 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Les3;
-
 Project projectReis = new Project("Reis", "op vakantie naar SPanje");
 Console.WriteLine(projectReis.ToString());
 projectReis.EditDescription("Reis","Op vakantie naar Turkije");
 Console.WriteLine(projectReis.ToString());
+
+MyQueue<int> queue = new MyQueue<int>();
+
+queue.Add(4);
+queue.Add(5);
+queue.Add(6);
+
+Console.WriteLine(queue.ToString());
+queue.Clear();
+
+
+MyQueue<Project> project1 = new MyQueue<Project>();
+
+// Adding projects to the queue
+project1.Add(new Project("Project1", "Work on task 1"));
+project1.Add(new Project("Project2", "Work on task 2"));
+project1.Add(new Project("Project3", "Work on task 3"));
+
+Console.WriteLine("Queue of Projects:");
+Console.WriteLine(project1.ToString());
+
+Console.WriteLine("\nNext project out of the queue:");
+Project nextProject = project1.NextOut();
+Console.WriteLine(nextProject.ToString());
+
+Console.WriteLine("\nRemaining queue of projects:");
+Console.WriteLine(project1.ToString());
