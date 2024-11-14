11ae41b0913c1da6cf8e02be6af3dbd20aa8ba9b
atiilla <9992685+atiilla@users.noreply.github.com>
2024-11-14 08:57:20 +0100

    updated

---
+++ programming-advanced/CodeReview.java
@@ -0,0 +1,15 @@
+public class CodeReview {
+    public static void main(String[] args) {
+        String name = "Aline";
+           
+        switch(name){
+            case "Alan": 
+                System.out.println("Almost");
+            case "Aline": 
+                System.out.println("Correct");
+                break;
+            default:
+                System.out.println("Nope");
+        }
+    }
+}
---
+++ programming-advanced/Conditions.java
@@ -0,0 +1,10 @@
+public class Conditions {
+    public static void main(String[] args) {
+        int A = 1;
+        int B = 2;
+        int C = 3;
+        if (A < B && B < C) {
+            System.out.println("Hoera, ik ben hier geraakt");
+        }
+    }
+}
